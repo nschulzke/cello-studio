f50d18b488c01c744f736ef9405cc4b3b0aa6ff9
Nathan Schulzke <nschulzke@gmail.com>
2018-05-17 21:01:45 -0600

    Remove unused createReducer function

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,21 +1,7 @@
 import { ActionType, AllActions } from "./actions";
-import { Index } from "modules/helpers";
 import { combineReducers, Reducer } from "redux";
 import { Session } from "./state";
 
-export function createReducer<T>(initialState: T, handlers: Index<Reducer<T>>): Reducer<T> {
-  return function reducer(state = initialState, action) {
-    if (handlers.hasOwnProperty(action.type)) {
-      return handlers[action.type](state, action)
-    } else {
-      return state
-    }
-  }
-}
-
-// ********
-// Reducers
-// ********
 export const session: Reducer<Session, AllActions> = (state = { loggedIn: false }, action) => {
   switch (action.type) {
     case ActionType.LOG_IN:
